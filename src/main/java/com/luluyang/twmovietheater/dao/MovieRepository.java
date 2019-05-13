147d435a9dc82fa7705434cd9c2f5414cae17a9a
Layla Wang <wanglei0717@outlook.com>
2019-05-13 23:23:59 +0800

    Add interface to connect to database

---
+++ src/main/java/com/luluyang/twmovietheater/dao/MovieRepository.java
@@ -0,0 +1,7 @@
+package com.luluyang.twmovietheater.dao;
+
+import com.luluyang.twmovietheater.model.Movie;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRepository extends CrudRepository<Movie, Integer> {
+}
