1e8479d8ec40a9a4a5327866ef1b0de1d4a041fe
Layla Wang <wanglei0717@outlook.com>
2019-05-14 10:54:41 +0800

    Add feature to get movie by id

--- src/main/java/com/luluyang/twmovietheater/dao/MovieController.java
+++ src/main/java/com/luluyang/twmovietheater/dao/MovieController.java
@@ -3,6 +3,7 @@ package com.luluyang.twmovietheater.dao;
 import com.luluyang.twmovietheater.model.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,12 @@ public class MovieController {
     @GetMapping("/movies")
     public Iterable<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    @GetMapping("/movies/{id}")
+    public Movie getMovieById(
+            @PathVariable("id") Integer id
+    ) {
+        return movieRepository.findById(id).get();
     }
 }
